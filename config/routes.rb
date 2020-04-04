5fca5b4fdcc0e3ef756b9092cff9dd0e6b340b75
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-04-04 21:41:45 +0900

    add new group function

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users , only: [:update,:destroy,:show,:edit]
+  resources :groups , only: [:create,:new]
 end
