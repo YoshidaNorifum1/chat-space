df5b151808b30602f92aa9d9858080e8b7eca775
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-04-05 16:16:14 +0900

    add routing for message

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users , only: [:update,:destroy,:show,:edit]
-  resources :groups , only: [:index,:create,:new,:edit,:update]
-  resources :posts, only: [:new,:create]
+  resources :groups , only: [:create,:new,:edit,:update] do
+    resources :messages, only: [:index,:create]
+  end
 end
