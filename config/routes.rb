3f90e727043df78777fbc60c50e99c59e81211a5
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-04-15 23:03:06 +0900

    view message on group

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :users , only: [:update,:destroy,:show,:edit]
   resources :groups , only: [:create,:new,:edit,:update] do
-    resources :messages, only: [:index,:create]
+    resources :messages, only: [:index,:create,:new]
   end
 end
