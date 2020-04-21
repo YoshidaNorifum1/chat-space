e22c5d0569f994844940d4fa6132232675a734a8
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-04-21 19:41:47 +0900

    add ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    formData = new FormData(this);
+    url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  });
 });
