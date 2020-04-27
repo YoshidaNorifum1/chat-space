605622ea0d3b1cdf14a8a7c000f2e0ec4c5d5a4d
YoshidaNorifum1 <flavor.of.coffee.and.cigar@gmail.com>
2020-04-27 12:51:13 +0900

    add incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,13 @@ class UsersController < ApplicationController
   def update
     redirect_to root_path
   end
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where('name LIKE ?', "%#{params[:keyword]}%").where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
 end
